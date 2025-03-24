68e4612fe410d29350b91054fc53c3b7525fbc1a
Nabeel <muhammadnabeeljavede@gmail.com>
2025-03-25 01:17:30 +0500

    Remove unnecessary console log from user API file

--- src/api/user.api.js
+++ src/api/user.api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 const USER_URL = import.meta.env.VITE_API_USERS_BASE_URL || "http://localhost:3000/api/users";
 
-console.log("Use Api Base URL: ", import.meta.env.VITE_API_USERS_BASE_URL)
 
 export const currentUser = async (id) => {
     const response = await axios.get(`${USER_URL}/${id}`);
